c46d93f6c4779268ecb69e27f0a9f85afd31d023
ahmedSobhi1122 <99087755+ahmedSobhi1122@users.noreply.github.com>
2023-02-26 20:30:23 +0200

    intial commit

---
+++ lib/intro.dart
@@ -0,0 +1,20 @@
+import 'dart:async';
+
+import 'package:duration_task/main.dart';
+import 'package:flutter/material.dart';
+
+class Intro extends StatelessWidget {
+  const Intro({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Timer(
+        const Duration(seconds: 5),
+        () => Navigator.of(context).pushReplacement(MaterialPageRoute(
+            builder: (BuildContext context) => const Duration_task())));
+    return const Scaffold(
+      backgroundColor: Color(0xff653AB4),
+      body:  Center(child: CircularProgressIndicator(color: Colors.white,)),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:duration_task/intro.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Intro(),
+    ),
+  );
+}
+
+class Duration_task extends StatelessWidget {
+  const Duration_task({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xff653AB4),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          GestureDetector(
+            onTap: null,
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Container(
+                height: 50,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(3),
+                    color: Colors.white),
+                child: const Center(
+                  child: Text('Generate'),
+                ),
+              ),
+            ),
+          ),
+          GestureDetector(
+            onTap: null,
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Container(
+                height: 50,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(3),
+                    color: Colors.white),
+                child: const Center(
+                  child: Text('Scan'),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
